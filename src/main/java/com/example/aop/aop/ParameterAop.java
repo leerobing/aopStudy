2d8204041d5aff6601f5534f5e94595198c43ae0
leerobing <105533691+leerobing@users.noreply.github.com>
2024-02-20 16:55:47 +0900

    feat: aop 공부 1

---
+++ src/main/java/com/example/aop/aop/ParameterAop.java
@@ -0,0 +1,37 @@
+package com.example.aop.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ParameterAop {
+
+    @Pointcut("execution(* com.example.aop.controller..*.*(..))")
+    public void cut() {
+
+    }
+
+    @Before("cut()")
+    public void beFore(JoinPoint joinPoint) {
+
+        Object[] args = joinPoint.getArgs();
+
+        for (Object obj : args) {
+            System.out.println("type : "+ obj.getClass().getSimpleName());
+            System.out.println("value : " + obj );
+        }
+    }
+
+    @AfterReturning(value = "cut()",returning = "returnObj")
+    public void afterReturn(JoinPoint joinPoint, Object returnObj) {
+
+        System.out.println("return obj");
+
+        System.out.println(returnObj);
+    }
+}
